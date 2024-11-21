dbe4fa308c8404c0ff237a3393a271cc29f083b1
m3lcy <shrilly3110@gmail.com>
2024-11-20 23:42:30 -0500

    Update send_device_config.py

--- send_device_config.py
+++ send_device_config.py
@@ -4,7 +4,7 @@ from netmiko import ConnectHandler
 HOST = "172.16.12.225" 
 USER = "cisco" 
 PASS = "cisco"
-TYPE = "cisco_xe"
+TYPE = "cisco_ios"
 
 r1 = ConnectHandler(host=HOST, username=USER, password=PASS, device_type=TYPE)
 
