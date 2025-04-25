eb55bbf30bd91c47a89f95a0a859213b69931ee2
root <root@m3lcy.>
2025-04-25 18:45:57 -0400

    added error handling

--- get_device_info.py
+++ get_device_info.py
@@ -1,18 +1,38 @@
-import netmiko
+from netmiko import ConnectHandler
 
-ip = '172.16.12.225'
-username = 'cisco'
+host = '192.168.12.36'
+username = 'admin'
 password = 'cisco'
 device_type = 'cisco_ios'
-port = 22 
+port = 22
+device_name = 'DLS1'
 
-net_connect = netmiko.ConnectHandler(
-    ip = ip,
-    device_type = device_type,
-    username = username,
-    password = password, 
-    port = port
- )
- 
- show_ip_route = net_connect.send_command('show ip route') 
- print(show_ip_route)
+net_connect = None
+
+try:
+    net_connect = ConnectHandler(
+        host = host,
+        username = username,
+        password = password,
+        device_type = device_type,
+        port = port,
+    )
+
+    show_ip_route = net_connect.send_command('show ip route')
+    print(f"Output for {device_name}: {show_ip_route}")
+
+    with open(f'{device_name}_ip_route.txt', 'w') as file:
+        file.write(f"Output for {device_name}: \n{show_ip_route}\n")
+
+except Exception as e:
+    print(f"Error {e}")
+
+finally: 
+    if net_connect:
+        net_connect.disconnect()
+        print(f"Disconnected from {device_name}")
+    else:
+        print(f"No connection to disconnect.")
+
+
+
--- send_device_config.py
+++ send_device_config.py
@@ -1,16 +1,15 @@
 from netmiko import ConnectHandler
+from netmiko.exceptions import NetmikoTimeoutException, NetmikoAuthenticationException
 
 
-HOST = "172.16.12.225" 
-USER = "cisco" 
-PASS = "cisco"
-TYPE = "cisco_ios"
+host = "192.168.12.36" 
+username = "admin" 
+password = "cisco"
+device_type = "cisco_ios"
+port = 22
+device_name = "dls1"
 
-dls1 = ConnectHandler(host=HOST, username=USER, password=PASS, device_type=TYPE)
-
-
-# Inside the python environment
-commands = [
+commands_vlan10 = [
     'interface vlan10',
     'ip address 172.16.12.1 255.255.255.224',
     'ip helper-address 172.16.12.226',
@@ -22,8 +21,50 @@ commands = [
     'no shutdown'
 ]
 
-dls1.send_config_set(commands)
+commands_vlan20 = [
+    'interface vlan20',
+    'ip address 172.16.12.33 255.255.255.224',
+    'ip helper-address 172.16.12.226',
+    'ip nat inside',
+    'standby version 2',
+    'standby 1 ip 172.16.12.254',
+    'standby 1 priority 150',
+    'standby 1 preempt',
+    'no shutdown'
+]
 
-dls1.send_command('write memory')
+dls1 = None
 
-dls1.disconnect()
+try:
+    dls1 = ConnectHandler(
+    host = host,
+    username = username,
+    password = password,
+    device_type = device_type,
+    port = port
+    )
+    print(f"Connected to {device_name}")
+
+    output_vlan10 = dls1.send_config_set(commands_vlan10)
+    print(f"VLAN 10 configuration sent.")
+    print(output_vlan10)
+
+    output_vlan20 = dls1.send_config_set(commands_vlan20)
+    print(f"VLAN 20 configuration sent.")
+    print(output_vlan20)
+
+    save_output = dls1.send_command('write memory')
+    print(f"Save Output:")
+    print(save_output)
+    
+except NetmikoAuthenticationException:
+    print(f"Authentication failed for {device_name} ({host})")
+except NetmikoTimeoutException:
+    print(f"Connection to {device_name} ({host}) timed out")
+except Exception as e:
+    print(f"An unexpected error occurred: {e}")
+finally:
+    if dls1: 
+        dls1.disconnect()
+        print(f"Disconnected from {device_name}")
+
