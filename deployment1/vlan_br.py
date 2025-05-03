ca941089600da3c662cca1c5ecf7476ee84e585c
root <root@m3lcy.>
2025-05-03 18:40:34 -0400

    .

--- deployment1/vlan_br.py
+++ deployment1/vlan_br.py
@@ -1,5 +1,6 @@
 from netmiko import ConnectHandler
 import yaml
+import os
 
 with open('vlans.yml') as file:
     vlans = yaml.safe_load(file)['vlans']
@@ -7,13 +8,15 @@ with open('vlans.yml') as file:
 with open('devices.yml') as file:
     devices = yaml.safe_load(file)['devices']
 
+os.makedirs("logs", exist_ok=True)
+
 try:
     for device in devices:
         net_connect = ConnectHandler(
-            host = {device['host']},
-            username = {device['username']},
-            password = {device['password']},
-            device_type = {device['device_type']}
+            host = device['host'],
+            username = device['username'],
+            password = device['password'],
+            device_type = device['device_type']
         )
 
         sh_vlan_br = net_connect.send_command('show vlan brief')
@@ -24,17 +27,16 @@ try:
                 vlan_id = str(vlan['id'])
                 vlan_name = vlan['name']
 
-            if vlan_id in sh_vlan_br:
-                print(f"[SKIPPED] VLAN already exists in {device['host']}")
-            else:
-                print(f"Creating VLAN on {device['host']}")
-                config_commands = [
-                    f"vlan {vlan_id}",
-                    f"name {vlan_name}"
-                ]
-
-                net_connect.send_config_set(config_commands)
-                file.write(f"[CREATED] VLAN {vlan_id} {vlan_name} created) on {device['host']}\n")
+                if vlan_id in sh_vlan_br:
+                    print(f"[SKIPPED] VLAN already exists in {device['host']}")
+                else:
+                    print(f"Creating VLAN on {device['host']}")
+                    config_commands = [
+                        f"vlan {vlan_id}",
+                        f"name {vlan_name}"
+                    ]
+                    net_connect.send_config_set(config_commands)
+                    file.write(f"[CREATED] VLAN {vlan_id} {vlan_name} created) on {device['host']}\n")
 
 except Exception as e:
     print("Could not connect to {device['host']}: Error {e}")
