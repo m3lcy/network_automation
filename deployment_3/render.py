a2beca8efc7c8fffdbe0dbb835e0a5bfd6d54e3d
root <root@m3lcy.>
2025-10-11 20:42:20 -0400

    .

--- deployment_3/render.py
+++ deployment_3/render.py
@@ -1,14 +1,46 @@
 from jinja2 import Environment, FileSystemLoader
+from datetime import datetime
+import logging
+import os
 import yaml
 
-with open('vlans.yml') as f:
-    vlans = yaml.safe_load(f)['vlans']
+timestamp = datetime.now().strftime("%Y%m%d_%H%M%S")
 
-env = Environment(loader=FileSystemLoader('.'))
-template = env.get_template('format.j2')
+os.makedirs('logs', exist_ok = True)
+os.makedirs('outputs', exist_ok = True)
 
-output = template.render(vlans=vlans)
+logging.basicConfig(
+    filename = 'logs/device_access.log',
+    level = logging.INFO,
+    format = '%(asctime)s = %(levelname)s - %(message)s'
+)
+
+try:
+    with open('data/vlans.yml') as f:
+        vlans = yaml.safe_load(f)['vlans']
+        logging.info(f"Loaded {len(vlans)} VLANS successfully.")
+except Exception as e:
+    logging.error(f"Failed to load VLANS: {e}")
+    raise
+
+try:
+    env = Environment(loader=FileSystemLoader('.'))
+    template = env.get_template('templates/template.j2')
+    output = template.render(vlans=vlans)
+    logging.info("Template rendered successfully.")
+except Exception as e:
+    logging.error(f"Template rendering failed: {e}")
 
 print(output)
-with open('generated_vlan_config.cfg', 'r') as f:
-    f.write(output)
+
+filename = f"outputs/generated_vlan_config.cfg"
+
+try:
+    with open('outputs/generated_vlan_config.cfg', 'w') as f:
+        f.write(output)
+    os.chmod(filename, 0o444)
+    logging.info(f"Configuration saved to {filename}")
+
+except Exception as e:
+    logging.error(f"Failed to write output: {e}")
+    raise
