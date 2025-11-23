1186d301a5465bc8b32244cdbe4f23e0f9bb0cdb
root <shrilly3110@gmail.com>
2025-11-23 00:34:54 -0500

    refactor: made code more modular

--- nornir_netmiko/scripts/gather_info.py
+++ nornir_netmiko/scripts/gather_info.py
@@ -6,7 +6,7 @@ from modules.logging_utils import setup_logging
 from modules.vault_utils import init_vault
 from modules.nornir_init import init_nornir
 from modules.credentials import load_credentials
-from tasks.gather_tasks import gather_info
+from nornir.tasks.gather_tasks import gather_info
 from nornir_utils.plugins.functions import print_result
 
 timestamp = setup_logging()
--- nornir_netmiko/scripts/send_config.py
+++ nornir_netmiko/scripts/send_config.py
@@ -6,7 +6,7 @@ from modules.logging_utils import setup_logging
 from modules.vault_utils import init_vault
 from modules.nornir_init import init_nornir
 from modules.credentials import load_credentials
-from tasks.config_tasks import send_config
+from nornir.tasks.config_tasks import send_config
 from nornir_utils.plugins.functions import print_result
 
 timestamp = setup_logging()
