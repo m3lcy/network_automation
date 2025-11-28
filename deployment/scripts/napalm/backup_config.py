4b8fd19c36fd53d424ec34d51d5c56e5a506ba0e
root <shrilly3110@gmail.com>
2025-11-27 21:11:24 -0500

    feat: add backup config using napalm for multi-vendors

---
+++ deployment/scripts/napalm/backup_config.py
@@ -0,0 +1,17 @@
+from modules.logging_utils import setup_logging
+from modules.vault_utils import init_vault
+from modules.nornir_init import init_nornir
+from modules.credentials import load_credentials
+from tasks.napalm.backup_config_tasks import backup_config
+from nornir_utils.plugins.functions import print_result
+
+timestamp = setup_logging()
+vault_client = init_vault()
+nr = init_nornir()
+load_credentials(nr, vault_client)
+
+result = nr.run(
+    task = backup_config,
+    timestamp = timestamp
+)
+print_result(result)
---
+++ deployment/tasks/napalm/backup_config_tasks.py
@@ -0,0 +1,33 @@
+from nornir_napalm.plugins.tasks import napalm_get
+from nornir.core.task import Result
+import logging
+import os
+import yaml
+
+def backup_config(task, timestamp):
+    try:
+        result = task.run(
+            task = napalm_get,
+            getters = ["config"]
+        )
+
+        config = result[0].result["config"]["running"]
+
+        filename = f"outputs/{task.host.name}_napalm_backup_{timestamp}.cfg"
+        with open(filename, "w") as f:
+            f.write(config)
+        os.chmod(filename, 0o444)
+        logging.info(f"Backup saved for {task.host.name} ({task.host.hostname}): {filename}")
+
+        return Result(
+            host = task.host,
+            result = f"Backup completed for {task.host.name} ({task.host.hostname})"
+        )
+
+    except Exception as e:
+        logging.error(f"Backups failed for {task.host.name} ({task.host.hostname}): {e}")
+        return Result(
+            host = task.host,
+            failed = True,
+            result = f"Backup failed: {e}"
+        )
